ef1d7a18a1936157f0535dd7e7658ce4b7bf72e0
Shanicky Chen <peng@risingwave-labs.com>
2024-02-23 18:30:09 +0800

    opt rcte
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/meta/src/controller/mod.rs
+++ src/meta/src/controller/mod.rs
@@ -34,11 +34,11 @@ pub mod fragment;
 pub mod id;
 pub mod rename;
 pub mod session_params;
+pub mod scale;
 pub mod streaming_job;
 pub mod system_param;
 pub mod user;
 pub mod utils;
-mod scale;
 
 // todo: refine the error transform.
 impl From<sea_orm::DbErr> for MetaError {
