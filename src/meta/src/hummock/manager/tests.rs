e260b4597d168ab7138c4ce5579f20f6ecba31e8
Little-Wallace <bupt2013211450@gmail.com>
2024-03-27 16:05:23 +0800

    batch report compact task
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/meta/src/hummock/manager/tests.rs
+++ src/meta/src/hummock/manager/tests.rs
@@ -43,7 +43,6 @@ use crate::hummock::compaction::selector::{
     default_compaction_selector, CompactionSelector, ManualCompactionOption,
     SpaceReclaimCompactionSelector,
 };
-use crate::hummock::compaction::CompactStatus;
 use crate::hummock::error::Error;
 use crate::hummock::test_utils::*;
 use crate::hummock::{CommitEpochInfo, HummockManager, HummockManagerRef};
@@ -1682,12 +1681,13 @@ async fn test_split_compaction_group_trivial_expired() {
         .unwrap();
     let mut selector: Box<dyn CompactionSelector> =
         Box::<SpaceReclaimCompactionSelector>::default();
-    let reclaim_task = hummock_manager
-        .get_compact_task_impl(2, &mut selector)
+    let normal_tasks = hummock_manager
+        .get_compact_tasks_impl(vec![2], &mut selector)
         .await
-        .unwrap()
         .unwrap();
-    assert!(CompactStatus::is_trivial_reclaim(&reclaim_task));
+    assert!(normal_tasks.is_empty());
+    // let reclaim_task = normal_tasks.pop().unwrap();
+    // assert!(CompactStatus::is_trivial_reclaim(&reclaim_task));
 
     let current_version = hummock_manager.get_current_version().await;
     let new_group_id = current_version.levels.keys().max().cloned().unwrap();
